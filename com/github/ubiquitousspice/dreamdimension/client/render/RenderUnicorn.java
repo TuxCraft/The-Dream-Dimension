d2413afdc8c89993fc58f795ac0aca0bfc50782b
zerorisers <blake.bowers.1@gmail.com>
2014-01-06 02:21:23 -0600

    rendering unicorn still the same.

--- com/github/ubiquitousspice/dreamdimension/client/render/RenderUnicorn.java
+++ com/github/ubiquitousspice/dreamdimension/client/render/RenderUnicorn.java
@@ -31,24 +31,24 @@ public class RenderUnicorn extends RenderLiving
         super(par1ModelBase, par2);
     }
 
-    protected void func_110847_a(EntityUnicorn par1EntityHorse, float par2)
+    protected void func_110847_a(EntityUnicorn par1EntityUnicorn, float par2)
     {
         float f1 = 1.0F;
 
         GL11.glScalef(f1, f1, f1);
-        super.preRenderCallback(par1EntityHorse, par2);
+        super.preRenderCallback(par1EntityUnicorn, par2);
     }
 
-    protected void func_110846_a(EntityUnicorn par1EntityHorse, float par2, float par3, float par4, float par5, float par6, float par7)
+    protected void func_110846_a(EntityUnicorn par1EntityUnicorn, float par2, float par3, float par4, float par5, float par6, float par7)
     {
-        if (par1EntityHorse.isInvisible())
+        if (par1EntityUnicorn.isInvisible())
         {
-            mainModel.setRotationAngles(par2, par3, par4, par5, par6, par7, par1EntityHorse);
+            mainModel.setRotationAngles(par2, par3, par4, par5, par6, par7, par1EntityUnicorn);
         }
         else
         {
-            func_110813_b(par1EntityHorse);
-            mainModel.render(par1EntityHorse, par2, par3, par4, par5, par6, par7);
+            func_110813_b(par1EntityUnicorn);
+            mainModel.render(par1EntityUnicorn, par2, par3, par4, par5, par6, par7);
         }	
     }
     protected ResourceLocation func_110863_a(EntityUnicorn par1EntityUnicorn)
@@ -85,6 +85,6 @@ public class RenderUnicorn extends RenderLiving
     @Override
     protected ResourceLocation getEntityTexture(Entity par1Entity)
     {
-        return this.func_110849_a((EntityUnicorn)par1Entity);
+        return location;
     }
 }
